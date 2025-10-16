300d89e3ac0510af590bc1581ab716a5780c55cc
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2025-10-16 10:22:52 +0300

    String methods example

--- part-01-basics/app.js
+++ part-01-basics/app.js
@@ -1,36 +1,17 @@
-// const örneği
-const sabitDeger = 10;
-console.log("Başlangıçta const:", sabitDeger);
+let title = "Hello World!";
 
-// const değerini değiştirmeye çalışalım (HATA ALIRIZ)
-// try içinde olmazsa hata olucaktır ve altadaki kodlar çalışmıcaktır.
+console.log(title.length);
 
-try {
-  sabitDeger = 20; // ❌ Bu satır hata verecek
-  console.log("Const değiştirildi:", sabitDeger);
-} catch (error) {
-  console.error("Const değiştirilemez! ");
-}
+console.log(title.toUpperCase());
 
+console.log(title.toLocaleLowerCase());
 
-// let örneği
-let degistirilebilirDeger = 5;
-console.log("Başlangıçta let:", degistirilebilirDeger);
+let index = title.indexOf("o")
+console.log(index);
 
-// let ile değişiklik yapalım (✅ bu geçerli)
-degistirilebilirDeger = 15;
-console.log("Let değiştirildi:", degistirilebilirDeger);
+let indexLast = title.lastIndexOf("o")
+console.log(indexLast);
 
-// Şimdi const’tan sonra let’i yanlış şekilde tanımlayalım
-// Yani aynı isimle önce const sonra let kullanmaya çalışalım
-try {
-  const test = 1;
-  console.log("Const test:", test);
-
-  // Aynı değişken ismini let ile tekrar tanımlayalım (❌ Hata verir)
-  let test = 2;
-  console.log("Let test:", test);
-} catch (error) {
-  console.error("Aynı isimle const ve let kullanılamaz! Hata:", error.message);
-}
+let updatedTitle = title.replace('!','.');
+console.log(updatedTitle);
 
